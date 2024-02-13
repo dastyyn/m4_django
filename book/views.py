12b0524f50e40277d13d6743f393fa637803f2f8
Дастан <dastyyyyy@icloud.com>
2024-02-13 15:02:19 +0600

    hw1

---
+++ book/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from . import models
+
+
+def books_list(request):
+    if request.method == 'GET':
+        books = models.Book.objects.all()
+        return render(request, 'books.html',
+                      context={'books': books})
