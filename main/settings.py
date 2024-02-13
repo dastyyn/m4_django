0ff17dbb4653e29b2b612eb84e6efc50b8b44577
Дастан <dastyyyyy@icloud.com>
2024-02-13 19:49:12 +0600

    токен вынесен в .env

--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 
@@ -8,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)4b)e90vju-6#ew&b2+#t#bca6v=6i^w2w(e&m(p)#8lfq9f*x'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Пока в разработке, статус True
